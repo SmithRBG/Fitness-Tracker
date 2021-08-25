1cbd571687a5b0110e90b7d42686d31ef41b6cd7
SmithRBG <smit36000@gmail.com>
2021-08-25 13:58:57 -0400

    models update

--- models/fitnessSchema.js
+++ models/fitnessSchema.js
@@ -1,11 +1,45 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const fitnessSchema = new fitness({
-
-
+const fitnessSchema = new Schema({
+    Name: {
+        type: String,
+        trim: true,
+        required: "Name is required"
+    },
+    Type: {
+        type: String,
+        trim: true,
+        required: "Type of Exercise is required"
+    },
+    Weight: {
+        type: Number,
+        required: false
+    },
+    Sets: {
+        type: Number,
+        required: false
+    },
+    Reps: {
+        type: Number,
+        required: false,
+    },
+    Duration: {
+        type: Number,
+        required: true,
+    },
+    Distance: {
+        type: Number,
+        required: false,
+    },
+    Date: {
+        type: Date,
+        default: Date.now
+    }
+    /* name, type, weight, sets, reps, and duration of exercise.
+    If the exercise is a cardio exercise, I should be able to track my distance traveled */
 });
 
-const tracker = mongoose.model("tracker", fitnessSchema);
+const Tracker = mongoose.model("tracker", fitnessSchema);
 
-module.exports = tracker;
+module.exports = Tracker;
--- server.js
+++ server.js
@@ -4,21 +4,23 @@ const morgan = require("morgan");
 
 const tracker = require("./models/fitnessSchema");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
+const PORT = process.env.PORT || 3000;
 
-/* const data = {
-  array: ["item1", "item2", "item3"],
-  boolean: false,
-  string:
-    "Something goes here...?",
-  number: 42
-}; */
+const app = express();
 
-/* Example.create(data)
-  .then(dbExample => {
-    console.log(dbExample);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
- */
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/#", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/#"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
