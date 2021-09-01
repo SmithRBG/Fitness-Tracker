0c59beb2614f3f28dc249c8c743f5b7955e0832d
SmithRBG <smit36000@gmail.com>
2021-09-01 08:42:38 -0400

    routes update

--- routes/jsroutes.js
+++ routes/jsroutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const fitnessSchema = require('./models/fitnessSchema');
+const fitnessSchema = require('./models/fitnessSchema.js');
 
-router.post('./jsroutes/fitnessSchema/index.js', (req, res) => {
+/* router.post('/api/workout', (req, res) => {
     fitnessSchema.create (body)
     .then(dbfitnessSchema => {
         res.json(dbfitnessSchema);
@@ -9,4 +9,33 @@ router.post('./jsroutes/fitnessSchema/index.js', (req, res) => {
     .catch(err => {
         res.status(400).json(err);
     });
+});
+
+router.get('./public/workout') */
+
+/* exports.index = function(req, res) {
+    fitnessSchema.find({}, function(err, docs) {
+        if(!err) {
+            res.json(200, {workouts:docs});
+        } else {
+            res.json(500, {message: err});
+        }
+    });
+} */
+
+
+router.get("/api/workouts", (req, res) => {
+    fitnessSchema.find({}).then(workoutInfo => {
+        console.log(workout)
+        res.json(workout)
+    })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    fitnessSchema.find({}).limit(10).then(workout => {
+        res.json(workout)
+    })
+    .catch(err => {
+        res.json(err)
+    })
 });
