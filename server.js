ac0dd987291bb43131ebbe5bb95715c88c0a174e
SmithRBG <smit36000@gmail.com>
2021-09-05 17:03:19 -0400

    deployment

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/#", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/FitnessTracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
